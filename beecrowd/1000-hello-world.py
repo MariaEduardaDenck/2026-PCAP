f187deb33aa412905dccfb24b45cf6b982c66f25
MariaEduardaDenck <20261ctb0100046@estudantes.ifpr.edu.br>
2026-04-07 18:12:42 +0000

    Aula 10 - 1000: Hello World

---
+++ beecrowd/1000-hello-world.py
@@ -0,0 +1,14 @@
+'''
+Problema: beecrowd: 1000
+Data: 2026.04.07
+Estudante: Maria Eduarda Denck
+'''
+#Objetivo: Exibir exatamente a mensagem
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: nenhuma - não há cálculo ou transformação
+# Saída: exibir o texto "Hello World!" exatamente como especificado
+
+# print() é o comando de saída em Python
+# Equivalente ao bloco "diga ..." do Scratch
+print('Hello World!') 
---
+++ maldicao.py
@@ -0,0 +1,9 @@
+print ("Hello World!")
+nome = input("Digite seu nome")
+print (f"olá, {nome}!")
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite outro valor: "))
+print(f"A soma de {valor1} + {valor2} é: {valor1+valor2}")
+
+
+
